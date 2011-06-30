ec0d4f5e8b76cb8c3c304d093eb7179e40777e6b
Jordi Noguera <jordinoguera83@gmail.com>
2011-06-30 15:02:10 +0100

    first commit

---
+++ hide_domains.js
@@ -0,0 +1,14 @@
+chrome.extension.sendRequest({data: "domain_list"}, function(response) {
+
+  domains = JSON.parse(response.data);
+
+  for (i = 0; i < domains.length; i++){
+    domain = domains[i].replace(/\./g, '\\.').replace(/\//g, '\\/')
+    if(document.location.href.match("^https?://(?:[^\/]*)?" + domain)){
+      document.location = chrome.extension.getURL('you_should_be_working.html');
+    }
+  }
+
+});
+
+
