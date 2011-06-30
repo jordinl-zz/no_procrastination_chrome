811d383aa1e76f411f57b4e81c66f69a1a074101
Jordi Noguera <jordinoguera83@gmail.com>
2011-06-30 16:55:26 +0100

    make js run as soon as possible

--- hide_domains.js
+++ hide_domains.js
@@ -1,14 +1,21 @@
-chrome.extension.sendRequest({data: "domain_list"}, function(response) {
+chrome.extension.sendRequest({ data: "is_paused" }, function(response) {
 
-  domains = JSON.parse(response.data);
-
-  for (i = 0; i < domains.length; i++){
-    domain = domains[i].replace(/\./g, '\\.').replace(/\//g, '\\/')
-    if(document.location.href.match("^https?://(?:[^\/]*)?" + domain)){
-      document.location = chrome.extension.getURL('you_should_be_working.html');
-    }
+  if(response.is_paused == true){
+    return;
   }
 
-});
+  chrome.extension.sendRequest({ data: "domain_list" }, function(response) {
 
+    domains = JSON.parse(response.data);
+
+    for (i = 0; i < domains.length; i++){
+      domain = domains[i].replace(/\./g, '\\.').replace(/\//g, '\\/')
+      if(document.location.href.match("^https?://(?:[^\/]*)?" + domain)){
+        document.location = chrome.extension.getURL('you_should_be_working.html');
+      }
+    }
+
+  });
+
+});
 
